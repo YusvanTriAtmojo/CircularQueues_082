97646cff860e31f3a6865a1d8f518194c2c7da85
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-06-23 15:17:05 +0700

    Jika elemen yang dihapus tidak berada di ujung terakhir maka FRONT +1 atau ke setelahnya

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -54,6 +54,8 @@ public:
 			//jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
 			if (FRONT == max - 1)
 				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
 		}
 	}
 };
